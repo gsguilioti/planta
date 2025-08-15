509ceba4680d117a4bb0e5f0f0a97ed4c92f486a
gsguilioti <gsguilioti@gmail.com>
2025-08-15 15:38:41 -0300

    starting rust project

---
+++ planta/target/debug/deps/planta-7bf2ecb6658b3fcd.d
@@ -0,0 +1,5 @@
+/home/giovanni/dev/planta/planta/target/debug/deps/planta-7bf2ecb6658b3fcd.d: src/main.rs
+
+/home/giovanni/dev/planta/planta/target/debug/deps/planta-7bf2ecb6658b3fcd: src/main.rs
+
+src/main.rs:
---
+++ planta/target/debug/planta.d
@@ -0,0 +1,1 @@
+/home/giovanni/dev/planta/planta/target/debug/planta: /home/giovanni/dev/planta/planta/src/main.rs
