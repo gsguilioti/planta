8e3f8f7caac7a6e26d5200f5c6cf91bc88dbe3b0
gsguilioti <gsguilioti@gmail.com>
2025-09-04 22:48:42 -0300

    creating generator structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::Read;
+
+mod generator;
+use crate::generator::Generator;
+
+fn main() -> std::io::Result<()>
+{
+    let mut lang_file = File::open("example/example.lang")?;
+    let mut lang_contents = String::new(); 
+    lang_file.read_to_string(&mut lang_contents)?;
+    
+    let generator = Generator::new();
+
+    Ok(())
 }
